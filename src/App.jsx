517f1c19c5fa57e614384b1fb67e93f3a31da538
Gloxiniaaa <hnm142758@gamil.com>
2025-03-12 00:28:04 +0700

    page: homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Home from "./pages/Home";
 
 function App() {
+  return <Home></Home>
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
